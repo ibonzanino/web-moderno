93c2f3484205a868bd7c42104e2c8bcb8036f8ff
Igor Bonzanino <igor.bonzanino@gmail.com>
2020-11-11 18:57:10 -0300

    initial commit

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,23 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = true
+
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.14
+
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 10
+
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar nomes genericos e siglas
+let valor = ''
+let numero = 1
+let pqp = false // Produto Químico Perigoso... kkk
---
+++ fundamentos/variavesEConstantes.js
@@ -0,0 +1,13 @@
+let a, b, c
+
+a = 1
+b = 2
+c = 3
+
+var d = 30
+
+console.log(a, b, c, d)
+
+const e = 50
+// e = 10
+console.log(e)
